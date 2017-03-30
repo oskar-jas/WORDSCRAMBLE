c3046d60d5240e28a31586a05439610982f3a07c
oskar-jas <15ojasiewicz@stjosephsrush.ie>
2017-03-30 15:16:46 +0100

    Update master code Kilo11.py

--- master code Kilo11.py
+++ master code Kilo11.py
@@ -1,3 +1,5 @@
-print("Welcome to random code generator")
-
+print("Welcome to random number generator")
+print("A number between 1-6 is given, the number is equivalent to the year group")
+print("The number that is given first is the year group that will leave their class at 12:48")
+print 
 import random
