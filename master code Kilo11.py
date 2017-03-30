b08c2afbd51176dc87483ab80f4eec8fd1eb6c21
oskar-jas <15ojasiewicz@stjosephsrush.ie>
2017-03-30 15:20:42 +0100

    Update master code Kilo11.py

--- master code Kilo11.py
+++ master code Kilo11.py
@@ -1,5 +1,9 @@
 print("Welcome to random number generator")
 print("A number between 1-6 is given, the number is equivalent to the year group")
 print("The number that is given first is the year group that will leave their class at 12:48")
-print 
+print("The next year group will leave at 12:51")
+print("Then 12:54")
+print("Followed by 12:57")
+print("And finally at 1:00")
+print("The first year group is....")
 import random
