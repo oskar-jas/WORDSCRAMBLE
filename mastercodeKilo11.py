e8c860713d45c78b4866c8fd52e2c0c5e6ba47d2
oskar-jas <15ojasiewicz@stjosephsrush.ie>
2017-04-25 15:33:03 +0100

    Slight change
    I decided that if we let 2 year groups out at once then the first class won't have a 12minute longer break but only 6minute

--- mastercodeKilo11.py
+++ mastercodeKilo11.py
@@ -1,20 +1,18 @@
 print("Welcome to random number generator")
 print("A number between 1-6 is given, the number is equivalent to the year group")
-print("The number that is given first is the year group that will leave their class at 12:48")
-print("The next year group will leave at 12:51")
-print("Then 12:54")
-print("Followed by 12:57")
-print("And finally at 1:00")
-print("The first year group is....")
+print("The number that is given first and second are the year group that will leave their class at 12:54")
+print("The next two year groups will leave at 12:57")
+print("And the last year groups at 1:00")
+print("The first two year group are....: ")
 import random
 for x in range(0, 6):
   random_number = random.randint(0, 6)
   print(random_number)
 """This is another code that gives a random number
-import random
 
-  number=random.random()
-  number=6*number
-  print(int(number))
+   import random
+   number=random.random()
+   number=6*number
+   print(int(number))
 """
 #Was trying to make a code that doesnt give 2 numbers twice but i havent made a successful one yet
