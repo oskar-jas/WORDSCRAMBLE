13d2f5ba7f3b3f729b50ed8b79d2692d7eea1e9e
oskar-jas <15ojasiewicz@stjosephsrush.ie>
2017-05-02 10:43:58 +0100

    added instructions
    i have added instructions so the person knows how to play the game

--- word-scramble.py
+++ word-scramble.py
@@ -1,4 +1,7 @@
+from time import sleep
 score = 0
+print("WELCOME TO....")
+sleep(2)
 print(" _    _  _________________ ")
 print("| |  | ||  _  | ___ |  _  |")
 print("| |  | || | | | |_/ / | | |")
@@ -12,3 +15,8 @@ print("\ `--.| /  \/| |_/ / /_\ \| .  . || |_/ / |    | |__  ")
 print(" `--. \ |    |    /|  _  || |\/| || ___ \ |    |  __| ")
 print("/\__/ / \__/\| |\ \| | | || |  | || |_/ / |____| |___ ")
 print("\____/ \____/\_| \_\_| |_/\_|  |_/\____/\_____/\____/ ")
+sleep(2)
+print("Make a word out of the letters given")
+print("You MUST use all of the letters given")
+print("If you make the correct word you will be awarded one point and will move on")
+print("If you get it wrong you will still move onto the next question")
